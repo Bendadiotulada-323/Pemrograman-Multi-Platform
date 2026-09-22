83b25ae7d1cd6d4ba7c0ae262e3e219b5bab0ed6
Bendadiotulada-323 <bendadiotulada@gmail.com>
2026-09-22 11:03:49 +0700

    Bagian 1 if else

---
+++ ControlSource.py
@@ -0,0 +1,14 @@
+Nilai = int(input("Masukan NIlaimu bendran dio"))
+
+if Nilai >= 90:
+    print("kamu sangat hebat ajarin aku dong")
+elif Nilai >= 80:
+    print("Good Job")
+elif Nilai >= 70:
+    print("Sudah cukup baik")
+elif Nilai >= 60:
+    print("Not Bad bung")
+else:
+    print("Semangat lagi belajarnya")
+
+number1 = int(input("Masukkan angka pertama"))
